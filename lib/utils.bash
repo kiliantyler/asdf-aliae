715692cbf5528f97040357caeff2e22c6f015753
Kilian Tyler <github@ktyler.me>
2024-09-13 22:36:36 -0400

    feat: fix lib

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for aliae.
 GH_REPO="https://github.com/jandedobbeleer/aliae"
 TOOL_NAME="aliae"
-TOOL_TEST="aliae --help"
+TOOL_TEST="aliae --version"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if aliae is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -68,6 +66,7 @@ download_release() {
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	chmod +x "$filename"
 }
 
 install_version() {
@@ -83,11 +82,10 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert aliae executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-
+		rm -rf "$ASDF_DOWNLOAD_PATH" || fail "Failed to remove the download directory."
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
 		rm -rf "$install_path"
